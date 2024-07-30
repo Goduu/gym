5fe3486969c254546ab7b5452a14f2c08ee7da08
Goduu <idcangussu@hotmail.com>
2024-07-30 21:37:55 +0200

    header css

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,7 +7,7 @@ export const Header = () => {
     return (
         <header className='py-7'>
             <Link href="/">
-                <Jyms className="h-20 mb-10 drop-shadow-lg dark:drop-shadow-[0_10px_8px_rgba(255,255,255,0.1)]" />
+                <Jyms className="h-20 mb-10 drop-shadow-lg dark:drop-shadow-[0_10px_8px_rgba(255,255,255,0.3)]" />
             </Link>
             <User />
         </header>
